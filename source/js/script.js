a81261b866ee4833579a3efd3080048d3091d3f2
BananaRepublic14 <mendiguzhaevea@gmail.com>
2021-05-15 14:28:42 +0300

    Доработка проекта после первой оценки

--- source/js/script.js
+++ source/js/script.js
@@ -12,3 +12,34 @@ navToggle.addEventListener("click", () => {
     showMenu.classList.add("show-menu");
   }
 });
+
+// Для редактора фотографий
+const workShopButtons = document.querySelectorAll(
+  ".workshop-photo__filter-button"
+);
+const workshopFilters = document.querySelectorAll(
+  ".workshop-photo__filter-item"
+);
+const activeButtonClassName = "workshop-photo__filter-button--active";
+const activeFilterClassName = "workshop-photo__filter-item--active";
+
+function toggleClassName(element, className) {
+  if (element.classList.contains(className)) {
+    return;
+  }
+  [...element.parentElement.children].forEach((child) =>
+    child.classList.remove(className)
+  );
+  element.classList.add(className);
+}
+
+workShopButtons.forEach(setButtonsHandler);
+
+function setButtonsHandler(button, i) {
+  button.addEventListener("click", () => {
+    toggleClassName(button, activeButtonClassName);
+    const currentFilter = workshopFilters[i];
+    toggleClassName(currentFilter, activeFilterClassName);
+    currentFilter.querySelector("input").focus();
+  });
+}
